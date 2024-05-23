d6a75bc6d31c247ce93c11bf28e48335dcc4cdd3
uttej-Gaian <uttejvarma.d@gaiansolutions.com>
2024-05-23 07:53:33 +0530

    handleing of some issues

--- inventory/src/pages/Home.jsx
+++ inventory/src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import Navbar from "../features/nav-bar/Navbar"
 import ProductList from "../features/productList/ProductList";
 import Footer from "../features/commonComponents/Footer";
-import ErrorFallback from "../features/commonComponents/errorFallback";
-import { ErrorBoundary } from "react-error-boundary";
 export default function Home(){
     return(
       <>
-        <ErrorBoundary fallback={ErrorFallback}>
+       
          <Navbar>
           <ProductList></ProductList>
          </Navbar>
          <Footer></Footer>
-         </ErrorBoundary> 
+         
       </>
     )
   }
